1800cfa4595e75359047fd6782ccb27cf4ccdd9c
BRADY COLLIER <S331401901@ocdsb>
2018-03-02 09:12:31 -0500

    Finished the final stages of getting the grid to update, next update will include the generations.

--- GOL/GOL.cpp
+++ GOL/GOL.cpp
@@ -1,11 +1,50 @@
 // GOL.cpp : main project file.
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
-using namespace System;
+using namespace std;
 
-int main(array<System::String ^> ^args)
-{
-    Console::WriteLine(L"Hello World");
-    return 0;
+int main(){
+	//Variables
+	int cell;
+	int grid[20][20];
+	int x[100];
+	int y[100];
+
+	//Initalize the first grid
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			grid[i][j] = 0;
+		}
+	}
+
+	cout << "How many cells would you like to make alive\n";
+	cin >> cell;
+	cout << "\n";
+
+	do {
+
+		//Drawing the second grid
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				cout << grid[i][j];
+			}
+			cout << "\n";
+		}
+		cout << "\nWhat cell would you like to have alive? (y,x)\n\n";
+		
+		//Placing alive cells
+		for (int i = 0; i < cell; i++) {
+				cin >> x[i] >> y[i];
+				x[i] = x[i] - 1;
+				y[i] = y[i] - 1;
+				grid[x[i]][y[i]] = 1;
+		}
+		system("CLS");
+
+	} 
+	while (true);
+	return 0;
 }
